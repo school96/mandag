ddd628495d67a43771832a7ed7501921a084c58f
school96 <williamban@viken.no>
2022-10-24 14:57:00 +0200

    Alle oppgaver ferdig

--- 1.py
+++ 1.py
@@ -18,7 +18,7 @@ salaryperday = basesalary / 30
 print("Etter 15 dager du vil har" + str(salaryperday * 15))
 print("Etter 30 dager du vil har" + str(salaryperday * 30))
 print("Etter 45 dager du vil har" + str(salaryperday * 45))
-print("Etter 50 dager du vil har") + str (salaryperday * 50)
+print("Etter 50 dager du vil har" + str(salaryperday * 50))
 
 ##Oppgave 2##
 ''' Opprett en funksjon som kan tegne følgende figur i terminalen:
@@ -45,7 +45,28 @@ print("Etter 50 dager du vil har") + str (salaryperday * 50)
  **********
 ***********
 '''
+def DrawShape():
+  startstring1 = "*"
+  for i in range(10):
+    print(startstring1)
+    startstring1 = startstring1 + "*"
+  startstring2 = "         *"
+  asterisks = 1
+  spaces = 9
+  for i in range(10):
+    print(startstring2)
+    startstring2 = ""
+    asterisks += 1
+    spaces -= 1
+    for i in range(spaces):
+      startstring2 = startstring2 + " "
+    for i in range(asterisks):
+      startstring2 = startstring2 + "*" 
+
+DrawShape()
 
 ##Oppgave 3##
 ''' Opprett en funksjon som kan printe alle primtall i variabel prime_numbers '''
 prime_numbers = [1, 5, 6, 7, 10, 11, 19, 23, 25, 26, 31, 26, 37, 40, 43, 67, 73, 99, 101]
+for i in range(len(prime_numbers)):
+  print(prime_numbers[i])
